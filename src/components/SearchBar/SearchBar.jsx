fe06440c3e6cda5b67eac128022bada4a9df58db
MadGenius16 <aleksandrco777@gmail.com>
2024-08-11 01:07:09 +0300

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,36 +3,43 @@ import css from "./SearchBar.module.css"
 import toast, { Toaster } from "react-hot-toast"
 import { FcSearch } from "react-icons/fc";
 
-const initial_Values = {query: ""}
+// const initial_Values = {query: ""}
 const notifyEmpty =()=> toast("text must be entered to search for images")
+
 const SearchBar = ({onSubmit}) => {
+
+ const handleSubmit=(values, actions)=>{
+    if(values.query.trim()==="") {
+      notifyEmpty()
+    } 
+    else{
+      onSubmit(values.query)
+      
+    }
+    actions.resetForm()
+  }
+  
+
+
   return (
     <Formik 
-    initialValues={initial_Values} 
-    onSubmit={(values, actions)=>{
-      if(values.query.trim()==="") {
-        notifyEmpty()
-      } 
-      else{
-        onSubmit(values.query)
-      }
-      actions.resetForm()
-    }
-    }>
+    initialValues={{query: ""}} 
+    onSubmit={handleSubmit}>
       <header className={css.container}>
   <Form className={css.form}>
     <Field className={css.field}
-    name="query"
+      name="query"
       type="text"
       placeholder="Search images and photos"
     />
-    <button className={css.button} type="submit"><FcSearch /></button>
+    <button 
+    className={css.button} 
+    type="submit"><FcSearch />
+    </button>
     <Toaster toastOptions={{style: {background: "red", color: "white"}}}/>
   </Form>
 </header>
     </Formik>
-    
-
   )
 }
 
